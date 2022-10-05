bb8e0dd1050e349d9b66563a027ef2046c77678a
Michael Huffman <mhuffman012@gmail.com>
2022-10-05 19:35:46 -0400

    Builds a list for later reference

--- main.go
+++ main.go
@@ -1,58 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+var cards [52]string
 
 func main() {
+	BuildDeck()
+	fmt.Printf("%+q\n", cards)
+}
 
-	var card string = "Ace of Spades"
-	fmt.Println(card)
+func BuildDeck() {
+	suits := map[string]string{
+		"diamonds": "red",
+		"hearts":   "red",
+		"clubs":    "black",
+		"spades":   "black",
+	}
 
-	// type Card_intf interface {
-	// 	Read()
-	// }
+	faces := map[string]int{
+		"ace":   1,
+		"two":   2,
+		"three": 3,
+		"four":  4,
+		"five":  5,
+		"six":   6,
+		"seven": 7,
+		"eight": 8,
+		"nine":  9,
+		"ten":   10,
+		"jack":  10,
+		"queen": 10,
+		"king":  10,
+	}
+	i := 0
+	for suit := range suits {
+		for face := range faces {
+			cards[i] = fmt.Sprintf("%s of %s", face, suit)
+			i++
+		}
 
-	// type face struct {
-	// 	name  string
-	// 	value int
-	// }
-
-	// type card struct {
-	// 	suit
-	// 	face
-	// }
-
-	// suits := map[string]string{
-	// 	"diamonds": "red",
-	// 	"hearts":   "red",
-	// 	"clubs":    "black",
-	// 	"spades":   "black",
-	// }
-
-	// faces := map[string]int{
-	// 	"ace":   1,
-	// 	"two":   2,
-	// 	"three": 3,
-	// 	"four":  4,
-	// 	"five":  5,
-	// 	"six":   6,
-	// 	"seven": 7,
-	// 	"eight": 8,
-	// 	"nine":  9,
-	// 	"ten":   10,
-	// 	"jack":  10,
-	// 	"queen": 10,
-	// 	"king":  10,
-	// }
-	// for suit in suits:
-	// m := map[string]Animals{
-	//     "cow":   Animals{SuperAnimals{"walk"}, "grass", "moo"},
-	//     "Cow":   Animals{SuperAnimals{"walk"}, "grass", "moo"},
-	//     "Bird":  Animals{SuperAnimals{"fly"}, "worms", "peep"},
-	//     "bird":  Animals{SuperAnimals{"fly"}, "worms", "peep"},
-	//     "Snake": Animals{SuperAnimals{"slither"}, "mice", "hsss"},
-	//     "snake": Animals{SuperAnimals{"slither"}, "mice", "hsss"},
-	// }
-
-	// suits := [4]string{"hearts", "diamonds", "spades", "clubs"}
-	// values := [13]string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"}
+	}
 }
