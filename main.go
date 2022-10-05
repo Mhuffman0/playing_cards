2dfb44bb931298011b168b579d69f3426a2d6829
Michael Huffman <mhuffman012@gmail.com>
2022-10-05 10:31:05 -0400

    first commit

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	type Card_intf interface {
+		Read()
+	}
+
+	type face struct {
+		name  string
+		value int
+	}
+
+	type card struct {
+		suit
+		face
+	}
+
+	suits := map[string]string{
+		"diamonds": "red",
+		"hearts":   "red",
+		"clubs":    "black",
+		"spades":   "black",
+	}
+
+	faces := map[string]int{
+		"ace":   1,
+		"two":   2,
+		"three": 3,
+		"four":  4,
+		"five":  5,
+		"six":   6,
+		"seven": 7,
+		"eight": 8,
+		"nine":  9,
+		"ten":   10,
+		"jack":  10,
+		"queen": 10,
+		"king":  10,
+	}
+	// for suit in suits:
+	// m := map[string]Animals{
+	//     "cow":   Animals{SuperAnimals{"walk"}, "grass", "moo"},
+	//     "Cow":   Animals{SuperAnimals{"walk"}, "grass", "moo"},
+	//     "Bird":  Animals{SuperAnimals{"fly"}, "worms", "peep"},
+	//     "bird":  Animals{SuperAnimals{"fly"}, "worms", "peep"},
+	//     "Snake": Animals{SuperAnimals{"slither"}, "mice", "hsss"},
+	//     "snake": Animals{SuperAnimals{"slither"}, "mice", "hsss"},
+	// }
+
+	// suits := [4]string{"hearts", "diamonds", "spades", "clubs"}
+	// values := [13]string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"}
+}
