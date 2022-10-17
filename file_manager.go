ab1e73d36399c173749f2b745a5b7762fb9916ab
Michael Huffman <mhuffman012@gmail.com>
2022-10-17 11:00:28 -0400

    Added build from deck functionality

--- file_manager.go
+++ file_manager.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"io/ioutil"
+	"log"
+	"os"
 	"strings"
 )
 
@@ -13,10 +14,29 @@ func (d deck) to_bytes() []byte {
 	return []byte(d.to_string())
 }
 
+
+
 func (d deck) saveToFile(filename string) error {
-	return ioutil.WriteFile(
-		filename,     // name of file
-		d.to_bytes(), // bytes to be written
-		0666,         // default file permissions world readable
+	current_dir, err := os.Getwd()
+	if err != nil {
+		log.Println(err)
+	}
+
+
+	return os.WriteFile(
+		current_dir+"\\"+filename, // name of file
+		d.to_bytes(),              // bytes to be written
+		0666,                      // default file permissions world readable
 	)
+}
+
+func readFromFile(filename string) (deck) {
+	bs, err := os.ReadFile(filename)
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+
+	deck:= strings.Split(string(bs), ", ")
+	return deck
 }
--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
+// import "fmt"
 func main() {
-	cards := buildDeck()
+	// cards := buildDeck()
 	// cards.print()
-	hand, cards := deal(cards, 5)
+	// hand, cards := deal(cards, 5)
 	// hand.print()
 	// cards.print()
 	// cards.to_string()
 	// hand.to_string()
-	hand.saveToFile("C:/Users/Mhuff/OneDrive/Documents/go_workspace/playing_cards/hand.txt")
+	// cards.saveToFile("cards.txt")
+	// hand.saveToFile("hand.txt")
+	cards := readFromFile("C:/Users/mhuff/OneDrive/Documents/go_workspace/playing_cards/cards.txt")
+	cards.print()
 }
