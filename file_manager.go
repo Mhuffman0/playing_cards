a0e7834a9420218430dd84ed33b1b1d7abebc6b5
Michael Huffman <mhuffman012@gmail.com>
2022-10-17 14:26:02 -0400

    Minor refactoring

--- file_manager.go
+++ file_manager.go
@@ -6,37 +6,42 @@ import (
 	"strings"
 )
 
-func (d deck) to_string() string {
-	return strings.Join([]string(d), ", ")
-}
-
 func (d deck) to_bytes() []byte {
-	return []byte(d.to_string())
+	s := []string(d)
+	return []byte(
+		strings.Join(
+			s,
+			", ",
+		))
 }
 
-
-
-func (d deck) saveToFile(filename string) error {
+func prependDirectory(filename string) string {
 	current_dir, err := os.Getwd()
 	if err != nil {
 		log.Println(err)
+		os.Exit(1)
 	}
+	return current_dir + "\\" + filename
+}
 
-
+func (d deck) saveToFile(filename string) error {
 	return os.WriteFile(
-		current_dir+"\\"+filename, // name of file
-		d.to_bytes(),              // bytes to be written
-		0666,                      // default file permissions world readable
+		prependDirectory(filename), // name of file
+		d.to_bytes(),               // bytes to be written
+		0666,                       // default file permissions world readable
 	)
 }
 
-func readFromFile(filename string) (deck) {
-	bs, err := os.ReadFile(filename)
+func readDeckFromFile(filename string) deck {
+	bs, err := os.ReadFile(prependDirectory(filename))
 	if err != nil {
 		log.Println(err)
 		os.Exit(1)
 	}
 
-	deck:= strings.Split(string(bs), ", ")
-	return deck
+	s := strings.Split(
+		string(bs),
+		", ",
+	)
+	return deck(s)
 }
